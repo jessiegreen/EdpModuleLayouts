2482660f3702b1da96c8506ed1ead92a15577ab6
Jessie Green <jessie.ims@gmail.com>
2014-03-21 21:46:03 -0500

    Add the ability to add a custom layout for a not found event using the "NotFound" key.

--- Module.php
+++ Module.php
@@ -1,15 +1,32 @@
 <?php
 namespace EdpModuleLayouts;
 
+use Zend\Mvc\MvcEvent, Zend\Mvc\Application;
+
 class Module
 {
     public function onBootstrap($e)
     {
-        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
+        $config       = $e->getApplication()->getServiceManager()->get('config');
+        $eventManager = $e->getApplication()->getEventManager();
+        
+        $eventManager->attach(
+            MvcEvent::EVENT_DISPATCH_ERROR, 
+            function(\Zend\Mvc\MvcEvent $e) use($config) {
+                if($e->getError() === Application::ERROR_ROUTER_NO_MATCH) {
+                    if(isset($config['module_layouts']['NotFound'])){
+                        $e->getViewModel()->setTemplate($config['module_layouts']['NotFound']);
+                    }
+                }          
+            }, 
+            100
+        );
+                
+        $eventManager->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) use($config) {
             $controller      = $e->getTarget();
             $controllerClass = get_class($controller);
             $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
-            $config          = $e->getApplication()->getServiceManager()->get('config');
+            
             if (isset($config['module_layouts'][$moduleNamespace])) {
                 $controller->layout($config['module_layouts'][$moduleNamespace]);
             }
